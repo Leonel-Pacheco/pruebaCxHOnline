5503a97936be68306e47d24270034c635e46b3fb
Leoo2002 <leonelquirogapacheco@gmail.com>
2024-12-09 15:08:30 -0300

    COMMIT

--- src/app/obtener-sheet.service.ts
+++ src/app/obtener-sheet.service.ts
@@ -6,22 +6,6 @@ declare const gapi: any;
   providedIn: 'root',
 })
 export class ObtenerSheetService {
-  async listMajors(sheetId: string,rango: string): Promise<Asesor[]> {
-
-    try {
-      const asesores: Asesor[] = [];
-        return asesores;
-      }
-     catch (err: any) {
-      console.error('Error fetching data:', err);
-      return [];
-    }
-  }
-}
-
-
-/* 
-export class ObtenerSheetService {
   API_KEY = 'AIzaSyAIwkB9yZMOv4GzzzQDO4PbvnQYRX4e2Kk';
   DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
 
@@ -91,7 +75,7 @@ export class ObtenerSheetService {
     }
   }
 }
-*/
+
 class Asesor {
   constructor(
     public nombre: string,
@@ -101,4 +85,4 @@ class Asesor {
     public transferidos: number,
     public TMO: number
   ) {}
-} 
+}
